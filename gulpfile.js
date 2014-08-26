03e14d85b89075c33da3e3d3cc42d83a2762a6ba
Ciaran Downey <me@ciarand.me>
2014-08-25 19:39:27 -0700

    Update gulpfile with uncss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require("gulp"),
     concat = require("gulp-concat"),
+    rename = require("gulp-rename"),
+    glob = require("glob"),
+    uncss = require("gulp-uncss"),
     minifycss = require("gulp-minify-css"),
-    css;
+    css = function (name) {
+        return "./css/" + name + ".css";
+    },
+    cssfiles = [css("poole"), css("lanyon"), css("syntax")];
 
-css = function (name) {
-    return "./css/" + name + ".css";
-};
+gulp.task("minifycss", function () {
+    glob("./_site/**/*.html", function (err, htmlfiles) {
+        if (err) {
+            throw err;
+        }
 
-gulp.task("css", function () {
-    gulp.src([css("poole"), css("lanyon"), css("syntax")])
-        .pipe(concat("style.css"))
-        .pipe(minifycss({}))
-        .pipe(gulp.dest("./css/"));
+        gulp.src(cssfiles)
+            .pipe(concat("style.css"))
+            .pipe(uncss({
+                html: htmlfiles
+            }))
+            .pipe(minifycss({}))
+            .pipe(gulp.dest("./css/"));
+    });
 });
 
-gulp.task("default", ["css"]);
+gulp.task("default", ["minifycss"]);
